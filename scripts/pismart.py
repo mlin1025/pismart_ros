55d8242c52cf38e62ab9e4018535148e33f7da5c
mlin1025 <mlin1025@terpmail.umd.edu>
2018-10-23 21:41:04 -0400

    motor speed should be int

--- scripts/pismart.py
+++ scripts/pismart.py
@@ -24,11 +24,8 @@ def motorCallback(speed):
     motorRight = max(min(speed.x,100),-100)
     motorLeft = max(min(speed.y,100),-100)
 
-    print motorRight
-    print motorLeft
-
-    motorA.speed = motorRight
-    motorB.speed = motorLeft
+    motorA.speed = (int)motorRight
+    motorB.speed = (int)motorLeft
 
 def listener():
     print "start listening"
